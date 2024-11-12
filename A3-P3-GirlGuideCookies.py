e06a77e5c36334e39680868dc3c929ca6315c273
Luc-brou <luc.r.brousseau@gmail.com>
2024-11-12 08:40:16 -0400

    Ver 1

--- A3-P3-GirlGuideCookies.py
+++ A3-P3-GirlGuideCookies.py
@@ -4,16 +4,49 @@
 #               those guides who go above and beyond in their sales efforts. The organizers
 #               want a program to print the guide list and their prizes.
 
-#Student #:     
-#Student Name:  
+#Student #:W0489720
+#Student Name: Luc Brousseau  
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
+    GuideNames=[]
+    GuidesBoxes=[]
 
+    Num=float(input("Input the number of Girl Guides selling cookies: "))
+    NumGuides=int(Num)
 
+    for counter in range(NumGuides):
+        GuideName=input(f"\nInput the name of girl guide #{counter+1}: ")
+        GuideBoxes=input(f"Input the amount of boxes sold by girl guide #{counter+1}: ")
+        GuideNames.append(GuideName)
+        GuidesBoxes.append(float(GuideBoxes))
 
+    AvgBoxes=((sum(GuidesBoxes))/NumGuides)
+    RoundAvgBoxes= round(AvgBoxes,0)
 
+    print("\n-------------------------------------------------------------------------------------------------------") 
+
+    #print(f"\nGuide {GuideNames} sold {GuidesBoxes} boxes") (neccesary? if so fix ordering and format of output)
+
+    #print("-------------------------------------------------------------------------------------------------------") 
+
+    if RoundAvgBoxes <0: #check if boxes are negative number(s), tell user
+        print("\nPlease enter valid numbers of boxes (No negative numbers).")
+
+    elif RoundAvgBoxes==0: #check for if no cookies sold, tell user
+        print("\nThe Girl Guides did not sell any cookies.")
+
+    #add an elif to show which girl guide didnt sell any boxes >:)
+
+    else: #Prints average amount of boxes sold
+        print(f"\nThe average amount of boxes sold by each Girl Guide is {int(RoundAvgBoxes)}.")
+
+    print("\n-------------------------------------------------------------------------------------------------------") 
+    
+    if (max(GuidesBoxes)):
+        print(f"\n{max(GuideNames)} win: trip to the Girl Guide Jamboree.")
+    
 
 
 
